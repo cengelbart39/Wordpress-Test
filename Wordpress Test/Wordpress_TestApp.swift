7ed1e11664e8d8e6b7d15f690028a385fb9f58c3
Christopher Engelbart <cengelbart39@protonmail.com>
2023-01-31 18:31:06 -0500

    Initial Commit

---
+++ Wordpress Test/Wordpress_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Wordpress_TestApp.swift
+//  Wordpress Test
+//
+//  Created by Christopher Engelbart on 1/31/23.
+//
+
+import SwiftUI
+
+@main
+struct Wordpress_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
